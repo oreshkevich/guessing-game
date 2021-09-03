613e11e65793c0c5da5acaf21bd36fdf913ac866
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-09-03 19:49:23 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
-
+     setRange(min, max) {
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.answer = Math.round((this.max + this.min) / 2);
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
